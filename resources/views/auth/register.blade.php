dac66774d883846280ecb82f19ef4a46a533949d
isamaulanatantra <isamaulanatantra@gmail.com>
2022-02-24 16:09:07 +0700

    registerviewbelumselesai

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,132 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <x-jet-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-jet-label for="terms">
-                        <div class="flex items-center">
-                            <x-jet-checkbox name="terms" id="terms"/>
-
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
-                                ]) !!}
+@extends('templates.back.main')
+@section('container')
+<nav class="navbar navbar-primary navbar-transparent navbar-absolute">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ url('/') }}">Material Dashboard Pro</a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class=" active ">
+                    <a href="{{ url('/register') }}">
+                        <i class="material-icons">person_add</i> Register
+                    </a>
+                </li>
+                <li class="">
+                    <a href="{{ url('/login') }}">
+                        <i class="material-icons">fingerprint</i> Login
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="wrapper wrapper-full-page">
+    <div class="full-page register-page" filter-color="black"
+        data-image="{{ asset('assets/back/assets/img/register.jpg') }}">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="card card-signup">
+                        <h2 class="card-title text-center">Register</h2>
+                        <div class="row">
+                            <div class="col-md-5 col-md-offset-1">
+                                <div class="card-content">
+                                    <div class="info info-horizontal">
+                                        <div class="icon icon-rose">
+                                            <i class="material-icons">timeline</i>
+                                        </div>
+                                        <div class="description">
+                                            <h4 class="info-title">Marketing</h4>
+                                            <p class="description">
+                                                We've created the marketing campaign of the website. It was a very
+                                                interesting collaboration.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="info info-horizontal">
+                                        <div class="icon icon-primary">
+                                            <i class="material-icons">code</i>
+                                        </div>
+                                        <div class="description">
+                                            <h4 class="info-title">Fully Coded in HTML5</h4>
+                                            <p class="description">
+                                                We've developed the website with HTML5 and CSS3. The client has access
+                                                to the code using GitHub.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="info info-horizontal">
+                                        <div class="icon icon-info">
+                                            <i class="material-icons">group</i>
+                                        </div>
+                                        <div class="description">
+                                            <h4 class="info-title">Built Audience</h4>
+                                            <p class="description">
+                                                There is also a Fully Customizable CMS Admin Dashboard for this product.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-5">
+                                <form class="form" method="" action="#">
+                                    <div class="card-content">
+                                        <div class="input-group">
+                                            <span class="input-group-addon">
+                                                <i class="material-icons">face</i>
+                                            </span>
+                                            <input type="text" class="form-control" placeholder="First Name...">
+                                        </div>
+                                        <div class="input-group">
+                                            <span class="input-group-addon">
+                                                <i class="material-icons">email</i>
+                                            </span>
+                                            <input type="text" class="form-control" placeholder="Email...">
+                                        </div>
+                                        <div class="input-group">
+                                            <span class="input-group-addon">
+                                                <i class="material-icons">lock_outline</i>
+                                            </span>
+                                            <input type="password" placeholder="Password..." class="form-control" />
+                                        </div>
+                                        <!-- If you want to add a checkbox to this form, uncomment this code -->
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="optionsCheckboxes" checked> I agree to the
+                                                <a href="#something">terms and conditions</a>.
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="footer text-center">
+                                        <a href="#pablo" class="btn btn-primary btn-round">Get Started</a>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-                    </x-jet-label>
+                    </div>
                 </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
-                </x-jet-button>
             </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+        <footer class="footer">
+            <div class="container">
+                <nav class="pull-left">
+                </nav>
+                <p class="copyright pull-right">
+                    &copy;
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>
+                    <a href="http://www.creative-tim.com/">Creative Tim</a>, made with love for a better web
+                </p>
+            </div>
+        </footer>
+    </div>
+</div>
+@endsection
---
+++ resources/views/templates/back/main.blade.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+
+
+<!-- Mirrored from demos.creative-tim.com/material-dashboard-pro/examples/pages/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 20 Mar 2017 21:32:19 GMT -->
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/back/assets/img/apple-icon.png') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('assets/back/assets/img/favicon.png') }}" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Material Dashboard PRO by Creative Tim | Premium Bootstrap Admin Template</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+    <!-- Canonical SEO -->
+    <link rel="canonical" href="//www.creative-tim.com/product/material-dashboard-pro" />
+    <!--  Social tags      -->
+    <meta name="keywords"
+        content="material dashboard, bootstrap material admin, bootstrap material dashboard, material design admin, material design, creative tim, html dashboard, html css dashboard, web dashboard, freebie, free bootstrap dashboard, css3 dashboard, bootstrap admin, bootstrap dashboard, frontend, responsive bootstrap dashboard, premiu material design admin">
+    <meta name="description"
+        content="Material Dashboard PRO is a Premium Material Bootstrap Admin with a fresh, new design inspired by Google's Material Design.">
+    <!-- Schema.org markup for Google+ -->
+    <meta itemprop="name" content="Material Dashboard PRO by Creative Tim | Premium Bootstrap Admin Template">
+    <meta itemprop="description"
+        content="Material Dashboard PRO is a Premium Material Bootstrap Admin with a fresh, new design inspired by Google's Material Design.">
+    <meta itemprop="image" content="../../../../s3.amazonaws.com/creativetim_bucket/products/51/opt_mdp_thumbnail.jpg">
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@creativetim">
+    <meta name="twitter:title" content="Material Dashboard PRO by Creative Tim | Premium Bootstrap Admin Template">
+    <meta name="twitter:description"
+        content="Material Dashboard PRO is a Premium Material Bootstrap Admin with a fresh, new design inspired by Google's Material Design.">
+    <meta name="twitter:creator" content="@creativetim">
+    <meta name="twitter:image"
+        content="../../../../s3.amazonaws.com/creativetim_bucket/products/51/opt_mdp_thumbnail.jpg">
+    <!-- Open Graph data -->
+    <meta property="fb:app_id" content="655968634437471">
+    <meta property="og:title" content="Material Dashboard PRO by Creative Tim | Premium Bootstrap Admin Template" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="http://www.creative-tim.com/product/material-dashboard-pro" />
+    <meta property="og:image"
+        content="../../../../s3.amazonaws.com/creativetim_bucket/products/51/opt_mdp_thumbnail.jpg" />
+    <meta property="og:description"
+        content="Material Dashboard PRO is a Premium Material Bootstrap Admin with a fresh, new design inspired by Google's Material Design." />
+    <meta property="og:site_name" content="Creative Tim" />
+    <!-- Bootstrap core CSS     -->
+    <link href="{{ asset('assets/back/assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="{{ asset('assets/back/assets/css/material-dashboard.css') }}" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="{{ asset('assets/back/assets/css/demo.css') }}" rel="stylesheet" />
+    <!--     Fonts and icons     -->
+    <link href="{{ asset('assets/back/assets/css/font-awesome.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/back/assets/css/google-roboto-300-700.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+    @yield('container')
+</body>
+<!--   Core JS Files   -->
+<script src="{{ asset('assets/back/assets/js/jquery-3.1.1.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/back/assets/js/jquery-ui.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/back/assets/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/back/assets/js/material.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/back/assets/js/perfect-scrollbar.jquery.min.js') }}" type="text/javascript"></script>
+<!-- Forms Validations Plugin -->
+<script src="{{ asset('assets/back/assets/js/jquery.validate.min.js') }}"></script>
+<!--  Plugin for Date Time Picker and Full Calendar Plugin-->
+<script src="{{ asset('assets/back/assets/js/moment.min.js') }}"></script>
+<!--  Charts Plugin -->
+<script src="{{ asset('assets/back/assets/js/chartist.min.js') }}"></script>
+<!--  Plugin for the Wizard -->
+<script src="{{ asset('assets/back/assets/js/jquery.bootstrap-wizard.js') }}"></script>
+<!--  Notifications Plugin    -->
+<script src="{{ asset('assets/back/assets/js/bootstrap-notify.js') }}"></script>
+<!--   Sharrre Library    -->
+<script src="{{ asset('assets/back/assets/js/jquery.sharrre.js') }}"></script>
+<!-- DateTimePicker Plugin -->
+<script src="{{ asset('assets/back/assets/js/bootstrap-datetimepicker.js') }}"></script>
+<!-- Vector Map plugin -->
+<script src="{{ asset('assets/back/assets/js/jquery-jvectormap.js') }}"></script>
+<!-- Sliders Plugin -->
+<script src="{{ asset('assets/back/assets/js/nouislider.min.js') }}"></script>
+<!--  Google Maps Plugin    -->
+<!--<script src="https://maps.googleapis.com/maps/api/js"></script>-->
+<!-- Select Plugin -->
+<script src="{{ asset('assets/back/assets/js/jquery.select-bootstrap.js') }}"></script>
+<!--  DataTables.net Plugin    -->
+<script src="{{ asset('assets/back/assets/js/jquery.datatables.js') }}"></script>
+<!-- Sweet Alert 2 plugin -->
+<script src="{{ asset('assets/back/assets/js/sweetalert2.js') }}"></script>
+<!--	Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+<script src="{{ asset('assets/back/assets/js/jasny-bootstrap.min.js') }}"></script>
+<!--  Full Calendar Plugin    -->
+<script src="{{ asset('assets/back/assets/js/fullcalendar.min.js') }}"></script>
+<!-- TagsInput Plugin -->
+<script src="{{ asset('assets/back/assets/js/jquery.tagsinput.js') }}"></script>
+<!-- Material Dashboard javascript methods -->
+<script src="{{ asset('assets/back/assets/js/material-dashboard.js') }}"></script>
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+<script src="{{ asset('assets/back/assets/js/demo.js') }}"></script>
+<script type="text/javascript">
+    $().ready(function () {
+        demo.checkFullPageBackgroundImage();
+
+        setTimeout(function () {
+            // after 1000 ms we add the class animated to the login/register card
+            $('.card').removeClass('card-hidden');
+        }, 700)
+    });
+</script>
+
+
+<!-- Mirrored from demos.creative-tim.com/material-dashboard-pro/examples/pages/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 20 Mar 2017 21:32:19 GMT -->
+
+</html>
