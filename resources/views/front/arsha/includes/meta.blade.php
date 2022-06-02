d5fdffcb76610003f1ecf5968c68658146a784ee
Isa Maulana Tantra <isamaulanatantra@gmail.com>
2022-06-02 07:38:33 +0700

    tambahkanmetatitle

--- resources/views/front/arsha/includes/meta.blade.php
+++ resources/views/front/arsha/includes/meta.blade.php
@@ -2,3 +2,5 @@
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 <meta content="{{ $data_website->web_description }}" name="description">
 <meta content="{{ $data_website->web_name }}" name="keywords">
+<meta content="{{ $data_website->web_name }}" name="title">
+<meta content="Isa Maulana Tantra" name="author">
