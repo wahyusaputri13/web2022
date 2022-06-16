807d6d4cfa34135c6b4fe2709fd6450e77b93180
Isa Maulana Tantra <isamaulanatantra@gmail.com>
2022-06-16 15:46:34 +0700

    ubahlinkhreffooter

--- resources/views/front/herobiz/includes/footer.blade.php
+++ resources/views/front/herobiz/includes/footer.blade.php
@@ -63,7 +63,7 @@
                     <!-- You can delete the links only if you purchased the pro version. -->
                     <!-- Licensing information: https://bootstrapmade.com/license/ -->
                     <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
-                    Develop by <a href="https://bootstrapmade.com/">Isa Maulana Tantra</a>
+                    Develop by <a href="#">Isa Maulana Tantra</a>
                 </div>
             </div>
 
