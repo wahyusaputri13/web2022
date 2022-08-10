c14020965ae1c65ca79a09da79298b3fc9a80c7d
pmahanani02 <puspitamahanani2@gmail.com>
2022-08-10 14:26:52 +0700

    tambahkan card

--- resources/views/front/pesonafm/pages/index.blade.php
+++ resources/views/front/pesonafm/pages/index.blade.php
@@ -36,6 +36,16 @@
                     </div>
                 </div>
         </div>
+        <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text" style="color: black;">Gallery</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+        </div>
+        
         <div class="row">
             <div class="col mt-3" style="color: white;">
                 hit counter website
