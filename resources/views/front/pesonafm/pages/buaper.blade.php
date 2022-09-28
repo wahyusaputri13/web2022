06b8ad7411fda7e6c66688e6dddd06cb19a075a3
pmahanani02 <puspitamahanani2@gmail.com>
2022-09-28 14:21:42 +0700

    update today

--- resources/views/front/pesonafm/pages/buaper.blade.php
+++ resources/views/front/pesonafm/pages/buaper.blade.php
@@ -1,5 +1,4 @@
-@extends('front.pesonafm.layouts.app')
-@section('content')
+
 <div class="max-w-sm rounded overflow-hidden shadow-lg">
   <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
   <div class="px-6 py-4">
@@ -14,4 +13,4 @@
     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
   </div>
 </div>
-@endsection
+
