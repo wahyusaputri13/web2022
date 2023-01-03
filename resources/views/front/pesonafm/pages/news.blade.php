e4f423f2df3bd183c807883656a1d02b8b139a97
soulofjava <isamaulanatantra@gmail.com>
2023-01-03 07:56:58 +0700

    comment header blade news

--- resources/views/front/pesonafm/pages/news.blade.php
+++ resources/views/front/pesonafm/pages/news.blade.php
@@ -10,7 +10,7 @@
 </style>
 <div class="container mx-auto py-1">
 
-  <div class="flex justify-between m-1 sm:mx-2">
+  <!-- <div class="flex justify-between m-1 sm:mx-2">
     <a href="{{url('/')}}">
       <h1 class="text-3xl font-bold text-center mx-10"> <span class="bg-gradient-to-r from-red-500 to-rose-400 bg-clip-text text-transparent"> 92.1 FM Radio
           Pesona</span> </h1>
@@ -21,7 +21,8 @@
       <a href="https://api.whatsapp.com/send?phone=6282323839996" class="hover:scale-125 text-red-500">Contact</a>
       <a href="{{ url('login') }}" class="hover:scale-125 text-red-500">Login</a>
     </div>
-  </div>
+  </div> -->
+
   <a href="{{url('/')}}">
     <h1 class="text-8xl font-bold text-center my-10"> Breaking <span class="bg-gradient-to-r from-blue-500 to-teal-400 bg-clip-text text-transparent">News Pesona</span> </h1>
   </a>
