838953dab8966169df47065fa38e40f332e71706
soulofjava <isamaulanatantra@gmail.com>
2023-01-03 10:35:03 +0700

    news blade

--- resources/views/front/pesonafm/pages/news.blade.php
+++ resources/views/front/pesonafm/pages/news.blade.php
@@ -1,86 +1,76 @@
 @extends('front.pesonafm.layouts.app')
 @section('content')
-
-<style>
-  .card-img-top {
-    width: 100%;
-    height: 15vw;
-    object-fit: cover;
-  }
-</style>
 <div class="container mx-auto py-1">
 
-  <!-- <div class="flex justify-between m-1 sm:mx-2">
-    <a href="{{url('/')}}">
-      <h1 class="text-3xl font-bold text-center mx-10"> <span class="bg-gradient-to-r from-red-500 to-rose-400 bg-clip-text text-transparent"> 92.1 FM Radio
-          Pesona</span> </h1>
-    </a>
-    <div class="flex space-x-4 font-medium sm:text-lg">
-      <a href="{{url('/newsall')}}" class="hover:scale-125  text-red-500">News</a>
-      <a href="{{url('/photos')}}" class="hover:scale-125 text-red-500">Tips & Trick</a>
-      <a href="https://api.whatsapp.com/send?phone=6282323839996" class="hover:scale-125 text-red-500">Contact</a>
-      <a href="{{ url('login') }}" class="hover:scale-125 text-red-500">Login</a>
+  <div class="flex mt-10">
+    <div class="basis-1/2 text-right">
+      <a href="{{url('/')}}" class="sm:text-4xl font-bold">
+        <h1> Breaking
+        </h1>
     </div>
-  </div> -->
-
-  <a href="{{url('/')}}">
-    <h1 class="text-8xl font-bold text-center my-10"> Breaking <span class="bg-gradient-to-r from-blue-500 to-teal-400 bg-clip-text text-transparent">News Pesona</span> </h1>
-  </a>
-  <a class="bg-red-500 px-10 py-2 w-20 h-30 text-white text-center rounded-lg"  href="{{ url('/') }}">Back</a>
-
-  <h3 class="sidebar-title text-black after:" style="margin-top: 20px;">Search</h3>
-  <div class="sidebar-item search-form text-black">
-    {{Form::open(['route' => 'news.search','method' => 'get', ''])}}
-    {{Form::text('kolomcari', null,['class' => 'form-control', 'placeholder' => 'Title Post'])}}
-    <button type="submit"><i class="bi bi-search text-black"></i></button>
-    {{Form::close()}}
+    <div class="basis-1/2">
+      <span class="bg-gradient-to-r from-blue-500 to-teal-400 bg-clip-text text-transparent">News
+        Pesona</span>
+      </a>
+    </div>
   </div>
+
+  <div class="flex justify-between mt-10">
+    <div class="grid place-items-center">
+      <div>
+        <a class="bg-red-500 px-10 py-2 w-20 h-30 text-white text-center rounded-lg" href="{{ url('/') }}">Back</a>
+      </div>
+    </div>
+    <div class="">
+      <div class="sidebar-item search-form text-black">
+        {{Form::open(['route' => 'news.search','method' => 'get', ''])}}
+        {{Form::text('kolomcari', null,['class' => 'form-control', 'placeholder' => 'Search Title Post'])}}
+        <button type="submit"><i class="bi bi-search text-black"></i></button>
+        {{Form::close()}}
+      </div>
+    </div>
+  </div>
+
   <!-- ======= News Section ======= -->
   <div class="container my-24 px-6 mx-auto">
-
-    <!-- Section: Design Block -->
     <section class="mb-32 text-gray-800">
       <div id="body" class="flex flex-wrap m-1 md:m-2">
-        @foreach($news as $n)
-        <div class="bottom-2 top-2 flex flex-wrap w-1/3 hover:grid-rows-6 mx-4 my-6 bg-gradient-to-r from-gray-100 to-gray-500 content-end rounded-md ">
-          <a href="{{ url('/news-detail', $n->slug) }}">
-            <div class="rounded-lg shadow-lg  max-w-sm">
-              @if(file_exists(public_path('storage/'.$n->path)))
-              <img src="{{ asset('storage') }}/{{ $n->path}}" class=" bg-blend-normal rounded-lg" alt="image" />
-              
-              @else
-              <img src="{{ asset('img/soulofjava.jpg') }}" class="img-fluid rounded-lg" alt="image"/>
-             
-              @endif
-              <div class="bottom-0 left-0 right-0 px-4 py-2 bg-gray-500 opacity-60 rounded-lg space-y-3">
-              <p class="text-center text-white z-10" style="top: 50%; left: 50%;">
-                {{ \Carbon\Carbon::parse($n->date)->format('l') }},
-                {{
-                  \Carbon\Carbon::parse( $n->date
-                 )->toFormattedDateString() }}
-              </p>
-              <h3 class="post-title text-white font-bold">{{ $n->title }}
-              </h3>
+        <div class="grid grid-cols-2 gap-4">
+          @foreach($news as $n)
+          <div
+            class="bottom-2 top-2 flex flex-wrap over:grid-rows-6 mx-4 my-6 bg-gradient-to-r from-gray-100 to-gray-500 content-end rounded-md ">
+            <a href="{{ url('/news-detail', $n->slug) }}">
+              <div class="rounded-lg shadow-lg  max-w-sm">
+                @if(file_exists(public_path('storage/'.$n->path)))
+                <img src="{{ asset('storage') }}/{{ $n->path}}" class=" bg-blend-normal rounded-lg" alt="image" />
+                @else
+                <img src="{{ asset('img/soulofjava.jpg') }}" class="img-fluid rounded-lg" alt="image" />
+                @endif
+                <div class="bottom-0 left-0 right-0 px-4 py-2 bg-gray-500 opacity-60 rounded-lg space-y-3">
+                  <p class="text-center text-white z-10" style="top: 50%; left: 50%;">
+                    {{ \Carbon\Carbon::parse($n->date)->format('l') }},
+                    {{
+                    \Carbon\Carbon::parse( $n->date
+                    )->toFormattedDateString() }}
+                  </p>
+                  <h3 class="post-title text-white font-bold">{{ $n->title }}
+                  </h3>
+                </div>
               </div>
-            </div>
-          </a>
+            </a>
+          </div>
+          @endforeach
         </div>
-
-        @endforeach
-      </div>
-
-      <div class="row mt-3" data-aos="fade-up" data-aos-delay="100">
-        <div class="col-lg-12 d-flex justify-content-center">
-          {{ $news->links('pagination::tailwind') }}
+        <div class="row mt-3">
+          <div class="col-lg-12 d-flex justify-content-center">
+            {{ $news->links('pagination::tailwind') }}
+          </div>
         </div>
       </div>
-
     </section>
     <!-- End News Section -->
-    
-    </div>
-  </div>  
-
+  </div>
+</div>
 @endsection
 @push('after-script')
 @endpush
--- resources/views/front/pesonafm/pages/newsbyauthor.blade.php
+++ resources/views/front/pesonafm/pages/newsbyauthor.blade.php
@@ -1,79 +1,82 @@
 @extends('front.pesonafm.layouts.app')
 @section('content')
-
-<style>
-    .card-img-top {
-        width: 100%;
-        height: 15vw;
-        object-fit: cover;
-    }
-</style>
 <div class="container mx-auto py-1">
-
-    <div class="flex justify-between m-1 sm:mx-2">
-        <a href="{{url('/')}}">
-            <h1 class="text-3xl font-bold text-center mx-10"> <span
-                    class="bg-gradient-to-r from-red-500 to-rose-400 bg-clip-text text-transparent"> 92.1 FM Radio
-                    Pesona</span> </h1>
-        </a>
-        <div class="flex space-x-4 font-medium sm:text-lg">
-            <a href="{{url('/newsall')}}" class="hover:scale-125  text-red-500">News</a>
-            <a href="{{url('/photos')}}" class="hover:scale-125 text-red-500">Tips & Trick</a>
-            <a href="https://api.whatsapp.com/send?phone=6282323839996" class="hover:scale-125 text-red-500">Contact</a>
-            <a href="{{ url('login') }}" class="hover:scale-125 text-red-500">Login</a>
-        </div>
-    </div>
-    <a href="{{url('/')}}">
+    <!-- <a href="{{url('/')}}">
         <h1 class="text-8xl font-bold text-center my-10"> Recent posts <span
                 class="bg-gradient-to-r from-blue-500 to-teal-400 bg-clip-text text-transparent">form our
                 Blog</span> </h1>
-    </a>
-    <h3 class="sidebar-title text-black">Search</h3>
-    <div class="sidebar-item search-form text-black">
-        {{Form::open(['route' => 'news.search','method' => 'get', ''])}}
-        {{Form::text('kolomcari', null,['class' => 'form-control', 'placeholder' => 'Title Post'])}}
-        <button type="submit"><i class="bi bi-search text-black"></i></button>
-        {{Form::close()}}
+    </a> -->
+    <div class="flex mt-10">
+        <div class="basis-1/2 text-right">
+            <a href="{{url('/')}}" class="sm:text-4xl font-bold">
+                <h1 class="font-bold text-right"> Recent posts </h1>
+        </div>
+        <div class="basis-1/2">
+            <span class="bg-gradient-to-r from-blue-500 to-teal-400 bg-clip-text text-transparent">from our
+                Blog</span>
+            </a>
+        </div>
     </div>
+
+    <div class="flex justify-between mt-10">
+        <div class="grid place-items-center">
+            <div>
+                <a class="bg-red-500 px-10 py-2 w-20 h-30 text-white text-center rounded-lg"
+                    href="{{ url('/') }}">Back</a>
+            </div>
+        </div>
+        <div class="">
+            <div class="sidebar-item search-form text-black">
+                {{Form::open(['route' => 'news.search','method' => 'get', ''])}}
+                {{Form::text('kolomcari', null,['class' => 'form-control', 'placeholder' => 'Search Title Post'])}}
+                <button type="submit"><i class="bi bi-search text-black"></i></button>
+                {{Form::close()}}
+            </div>
+        </div>
+    </div>
+
     <!-- ======= News Section ======= -->
     <section id="news" class="portfolio">
         <div class="sidebar">
             <div class="container px-3 py-2 mx-auto lg:pt-24 lg:px-32">
                 <div id="body" class="flex flex-wrap m-1 md:m-2">
-                    @foreach($data as $n)
-                    <div class="flex flex-wrap w-1/3">
-                        <div class="w-full p-1 md:p-2">
-                            <div class="relative overflow-hidden bg-no-repeat  bg-white bg-cover shadow-lg rounded-lg"
-                                style="background-position: 50%;" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                                <div class="rounded-lg shadow-lg  max-w-sm">
-                                    @if(file_exists(public_path('storage/'.$n->path)))
-                                    <img src="{{ asset('storage') }}/{{ $n->path}}" class="card-img-top" alt="image">
-                                    @else
-                                    <img src="{{ asset('img/soulofjava.jpg') }}" class="img-fluid" alt="image">
-                                    @endif
+                    <div class="grid grid-cols-2 gap-4">
+                        @foreach($data as $n)
+                        <div class="flex flex-wrap">
+                            <div class="w-full p-1 md:p-2">
+                                <div class="relative overflow-hidden bg-no-repeat  bg-white bg-cover shadow-lg rounded-lg"
+                                    style="background-position: 50%;" data-mdb-ripple="true"
+                                    data-mdb-ripple-color="light">
+                                    <div class="rounded-lg shadow-lg  max-w-sm">
+                                        @if(file_exists(public_path('storage/'.$n->path)))
+                                        <img src="{{ asset('storage') }}/{{ $n->path}}" class="card-img-top"
+                                            alt="image">
+                                        @else
+                                        <img src="{{ asset('img/soulofjava.jpg') }}" class="img-fluid" alt="image">
+                                        @endif
+                                    </div>
+                                    <span class="post-date text-black">{{ \Carbon\Carbon::parse($n->date)->format('l')
+                                        }},
+                                        {{
+                                        \Carbon\Carbon::parse( $n->date
+                                        )->toFormattedDateString() }}</span>
+                                    <h3 class="post-title text-black">{{ $n->title }}
+                                    </h3>
+                                    <a href="{{ url('/news-detail', $n->slug) }}"
+                                        class="readmore stretched-link mt-auto text-black"><span>Read
+                                            More</span><i class="bi bi-arrow-right"></i></a>
                                 </div>
-                                <span class="post-date text-black">{{ \Carbon\Carbon::parse($n->date)->format('l') }},
-                                    {{
-                                    \Carbon\Carbon::parse( $n->date
-                                    )->toFormattedDateString() }}</span>
-                                <h3 class="post-title text-black">{{ $n->title }}
-                                </h3>
-                                <a href="{{ url('/news-detail', $n->slug) }}"
-                                    class="readmore stretched-link mt-auto text-black"><span>Read
-                                        More</span><i class="bi bi-arrow-right"></i></a>
                             </div>
+
                         </div>
-
+                        @endforeach
                     </div>
-                    @endforeach
                 </div>
-
                 <div class="row mt-3" data-aos="fade-up" data-aos-delay="100">
                     <div class="col-lg-12 d-flex justify-content-center">
                         {{ $data->links('pagination::tailwind') }}
                     </div>
                 </div>
-
     </section>
     <!-- End News Section -->
     @endsection
