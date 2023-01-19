31b2e05975155ff892d569e39d1ee673f25823f8
soulofjava <isamaulanatantra@gmail.com>
2023-01-19 09:56:31 +0700

    clear png navbar

--- resources/views/front/anada/includes/header.blade.php
+++ resources/views/front/anada/includes/header.blade.php
@@ -43,8 +43,10 @@
                     <i class="fa fa-bars"></i>
                 </button>
                 <a class="navbar-brand" href="index.html">
-                    <img src="{{ asset('assets/front/anada/assets/img/logo-light.png') }}" class="logo default" alt="Logo">
-                    <img src="{{ asset('assets/front/anada/assets/img/logo.png') }}" class="logo logo-responsive" alt="Logo">
+                    <img src="{{ asset('assets/front/anada/assets/img/logo.png') }}" class="logo default" alt="Logo">
+                    <!-- logo saat tampilan hp -->
+                    <img src="{{ asset('assets/front/anada/assets/img/logo-white.png') }}" class="logo logo-responsive"
+                        alt="Logo">
                 </a>
             </div>
             <!-- End Header Navigation -->
