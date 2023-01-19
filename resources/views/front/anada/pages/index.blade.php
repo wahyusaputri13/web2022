8b008f5a8bd3fc9ecdb4c019abf239e286d44c24
soulofjava <isamaulanatantra@gmail.com>
2023-01-19 13:33:24 +0700

    update

--- resources/views/front/anada/pages/index.blade.php
+++ resources/views/front/anada/pages/index.blade.php
@@ -221,4 +221,6 @@
 <!-- End Google Maps -->
 @endsection
 @push('after-script')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 @endpush
