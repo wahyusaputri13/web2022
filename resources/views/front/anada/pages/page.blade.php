57c9bfa74047470121c84f7d9b53f8aeb68f76f6
soulofjava <isamaulanatantra@gmail.com>
2023-01-20 12:33:32 +0700

    page keberatan anada

--- resources/views/front/anada/pages/page.blade.php
+++ resources/views/front/anada/pages/page.blade.php
@@ -29,7 +29,117 @@
                 <div class="card mb-3">
                     <div class="card-body">
                         <div class="card-content">
+                            @if($data->menu_name == 'Permohonan Informasi Publik')
+                            <div id="contact" class="contact-area default-padding">
+                                <div class="container">
+                                    <div class="contact-items">
+                                        <div class="row">
+                                            <div class="col-lg-12 right-item">
+                                                {{Form::open(['url' => '#','method' => 'post', 'files' =>
+                                                'true', '', 'class' =>
+                                                'php-email-form'])}}
+                                                <div class="row">
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            {{Form::text('name', null,['class' => 'form-control',
+                                                            'placeholder' => 'Nama',
+                                                            'required'])}}
+                                                            <span class="alert-error"></span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            {{Form::text('name', null,['class' => 'form-control',
+                                                            'placeholder' => 'Alamat',
+                                                            'required'])}}
+                                                            <span class="alert-error"></span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            {{Form::email('email', null,['class' => 'form-control',
+                                                            'placeholder' => 'Email',
+                                                            'required'])}}
+                                                            <span class="alert-error"></span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            {{Form::number('phone', null,['class' => 'form-control',
+                                                            'placeholder' =>
+                                                            'Nomor Handphone',
+                                                            'required'])}}
+                                                            <span class="alert-error"></span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            {{Form::text('name', null,['class' => 'form-control',
+                                                            'placeholder' => 'Pekerjaan',
+                                                            'required'])}}
+                                                            <span class="alert-error"></span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group comments">
+                                                            {{Form::textarea('message', null,['class' => 'form-control',
+                                                            'placeholder' => 'Tujuan Penggunaan Informasi',
+                                                            'required'])}}
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group comments">
+                                                            {{Form::textarea('message', null,['class' => 'form-control',
+                                                            'placeholder' => 'Rincian Informasi Yang Diinginkan',
+                                                            'required'])}}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="form-group">
+                                                        <div class="row">
+                                                            <div class="col-sm-12 col-md-12 captcha mb-3">
+                                                                <span>{!! captcha_img() !!}</span>
+                                                                <button type="button" class="btn btn-danger"
+                                                                    class="reload" id="reload">
+                                                                    &#x21bb;
+                                                                </button>
+                                                            </div>
+                                                            <div class="col-sm-12 col-md-12 mb-3">
+                                                                {{Form::number('captcha', null,['class' =>
+                                                                'form-control',
+                                                                'placeholder' => 'Enter Captcha Result',
+                                                                'required'])}}
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-lg-12">
+                                                        <center>
+                                                            <button type="submit">
+                                                                Kirim Permohonan <i class="fa fa-paper-plane"></i>
+                                                            </button>
+                                                        </center>
+                                                    </div>
+                                                </div>
+                                                {{Form::close()}}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            @elseif($data->menu_name == 'Pengajuan Keberatan Informasi Publik')
+                            keberatan
+                            @else
                             {!! $data->content !!}
+                            @endif
                         </div>
                     </div>
                 </div>
