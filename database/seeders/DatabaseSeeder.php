7369eb6b8f49b1139937b36deac0c7085b15e65a
ThinkTanker <isamaulanatantra@gmail.com>
2023-06-13 08:12:16 +0700

    perbaikan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,28 +24,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call([
-        // NewsSeeder::class,
         // GallerySeeder::class,
         // ]);
-
-        $themes = [
-            [
-                'name' => 'flexstart',
-                'image' => 'img/flexstart.png'
-            ],
-            [
-                'name' => 'herobiz',
-                'image' => 'img/herobiz.png'
-            ],
-            [
-                'name' => 'arsha',
-                'image' => 'img/arsha.png'
-            ]
-        ];
-
-        foreach ($themes as $datum) {
-            Themes::create($datum);
-        }
 
         $role = [
             [
@@ -60,46 +40,20 @@ class DatabaseSeeder extends Seeder
             Role::create($datum);
         }
 
-        $front_menu = [
-            [
-                'menu_name' => 'Main Menu'
-            ]
-        ];
-
-        foreach ($front_menu as $ddd) {
-            FrontMenu::create($ddd);
-        }
-
-        // \App\Models\User::factory(10)->create();
-        // DB::table('websites')->insert([
-        //     'web_name' => 'Web2022',
-        //     'web_description' => '"Hello World!"',
-        //     'email' => 'diskominfo@wonosobokab.go.id',
-        //     'address' => 'Wonosobo - The Soul Of Java',
-        //     'phone' => '085643710007',
-        //     'instagram' => '#',
-        //     'twitter' => '#',
-        //     'facebook' => '#',
-        //     'youtube' => '#',
-        //     'url_stream' => '#',
-        //     'themes_front' => 'arsha',
-        //     'themes_back' => 'back.a',
-        //     'open_hours' => 'Monday - Thursday (07:00AM - 04:00PM) Friday (07:00AM - 11:00AM)',
-        // ]);
-        // DB::table('websites')->insert([
-        //     'web_name' => 'KAMPUNG PANCASILA WONOSOBO',
-        //     'web_description' => '"Semarak Kampung Pancasila Wonosobo penjaga Persatuan dan Kesatuan Bangsa!"',
-        //     'email' => 'superadmin@app.com',
-        //     'address' => 'Kodim 0707 Jl. Pemuda No.11, Wonosobo Timur, Wonosobo Tim., Kec. Wonosobo, Kabupaten Wonosobo, Jawa Tengah 56311',
-        //     'phone' => '(0286) 321019',
-        //     'instagram' => '#',
-        //     'twitter' => '#',
-        //     'facebook' => '#',
-        //     'youtube' => '#',
-        //     'url_stream' => '#',
-        //     'themes_front' => 'FlexStart',
-        //     'themes_back' => 'back.a',
-        // ]);
+        DB::table('websites')->insert([
+            'web_name' => 'PesonaFM',
+            'web_description' => '92.1 FM Radio Pesona Stasiun Terbaikmu',
+            'email' => 'superadmin@app.com',
+            'address' => 'Jl. Soekarno - Hatta No.12A, Wonosobo Timur, Wonosobo Tim., Kec. Wonosobo, Kabupaten Wonosobo, Jawa Tengah 56311',
+            'phone' => '(0286)',
+            'instagram' => '#',
+            'twitter' => '#',
+            'facebook' => '#',
+            'youtube' => '#',
+            'url_stream' => '#',
+            'themes_front' => 'FlexStart',
+            'themes_back' => 'back.a',
+        ]);
 
         $user = [
             [
@@ -110,7 +64,7 @@ class DatabaseSeeder extends Seeder
             ],
             [
                 'name' => 'admin',
-                'email' => 'admin@app.com',
+                'email' => 'pesona@app.com',
                 'password' => bcrypt('password'),
                 'role_id' => 2
             ]
@@ -118,48 +72,6 @@ class DatabaseSeeder extends Seeder
 
         foreach ($user as $datum) {
             User::create($datum);
-        }
-
-        $related = [
-            [
-                'name' => 'Website Pemkab Wonosobo',
-                'url' => 'https://website.wonosobokab.go.id/',
-            ],
-            [
-                'name' => 'Dashboard Smartcity',
-                'url' => 'https://smartcity.wonosobokab.go.id/',
-            ],
-            [
-                'name' => 'Website Diskominfo Wonosobo',
-                'url' => 'https://diskominnfo.wonosobokab.go.id/',
-            ]
-        ];
-
-        foreach ($related as $rr) {
-            RelatedLink::create($rr);
-        }
-
-        $component = [
-            [
-                'name' => 'Guest Book',
-                'active' => 0,
-                'slug' => Str::slug('Guest Book', '-'),
-            ]
-        ];
-
-        foreach ($component as $cp) {
-            Component::create($cp);
-        }
-
-        $faker = Faker::create('id_ID');
-        for ($i = 1; $i <= 50; $i++) {
-            GuestBook::create([
-                'name'  => $faker->name(),
-                'date'  => $faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now'),
-                'instansi'  => $faker->company(3),
-                'jumlah'  => $faker->randomDigit(),
-                'keperluan'  => $faker->sentence()
-            ]);
         }
     }
 }
