c67e214843b161adac1c0ebfdf7e5e64c2f45993
isamaulanatantra <isamaulanatantra@gmail.com>
2023-07-26 09:22:58 +0700

    tambah iklan

--- resources/views/front/pesonafm/layouts/app.blade.php
+++ resources/views/front/pesonafm/layouts/app.blade.php
@@ -10,6 +10,14 @@
 
 <body class="bg-fixed bg-no-repeat bg-cover font-ubuntu text-white semok">
     @include('front.pesonafm.includes.header')
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4623751537139602"
+        crossorigin="anonymous"></script>
+    <!-- Pesona Dashboards -->
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-4623751537139602" data-ad-slot="3106864972"
+        data-ad-format="auto" data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
     @yield('content')
     @include('front.pesonafm.includes.footer')
     @stack('before-script')
