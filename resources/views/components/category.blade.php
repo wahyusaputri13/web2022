968e420f41a2408f65c3560855f67223100f5885
isamaulanatantra <isamaulanatantra@gmail.com>
2023-08-14 14:17:09 +0700

    perbaikan kategori

--- resources/views/components/category.blade.php
+++ resources/views/components/category.blade.php
@@ -1,12 +1,19 @@
 <h3 class="{{ $h3 }}">Categories</h3>
 <div class="{{ $div }}">
     <ul>
-        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_4">Berita <span>({{ $berita }})</span></a></li>
-        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_1">Dokumentasi <span>({{ $dokumentasi }})</span></a>
+        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_4">Umum <span>({{ $berita }})</span></a></li>
+        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_1">
+                Kesejahtaraan Rakyat<span>({{ $dokumentasi }})</span></a>
         </li>
-        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_3">Notulensi <span>({{ $notulensi}})</span></a></li>
-        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_2">Press Release <span>({{ $press }})</span></a></li>
-        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_0">Sambutan <span>({{ $sambutan }})</span></a></li>
+        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_3">Pemerintahan <span>({{ $notulensi}})</span></a></li>
+        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_2">Protokol dan Komunikasi Pimpinan <span>({{ $press
+                    }})</span></a></li>
+        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_0">Administrasi Pembangunan <span>({{ $sambutan
+                    }})</span></a></li>
+        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_4">Organisasi <span>({{ $berita }})</span></a></li>
+        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_4">Pengadaan Barang dan Jasa <span>({{ $berita
+                    }})</span></a></li>
+        <li><a href="{{ url('newscategory') }}/KATEGORI_NEWS_4">Hukum <span>({{ $berita }})</span></a></li>
     </ul>
 </div>
 <!-- End sidebar categories -->
