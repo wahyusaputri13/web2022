647eb37a95a93ecae2489ebc5e7722e2bbb2d039
isamaulanatantra <isamaulanatantra@gmail.com>
2023-08-19 08:04:07 +0700

    rapikan blade index

--- resources/views/front/boxass/layouts/app.blade.php
+++ resources/views/front/boxass/layouts/app.blade.php
@@ -10,17 +10,15 @@
 </head>
 
 <body>
-    <main>
-        <a href="#" id="websitename" hidden>{{ $data_website->web_name }}</a>
-        @include('sweetalert::alert')
-        @include('front.boxass.includes.header')
-        @yield('content')
-        @include('front.boxass.includes.footer')
-        @stack('before-script')
-        @include('front.boxass.includes.script')
-        @include('components.responsive-voice')
-        @stack('after-script')
-    </main>
+    <a href="#" id="websitename" hidden>{{ $data_website->web_name }}</a>
+    @include('sweetalert::alert')
+    @include('front.boxass.includes.header')
+    @yield('content')
+    @include('front.boxass.includes.footer')
+    @stack('before-script')
+    @include('front.boxass.includes.script')
+    @include('components.responsive-voice')
+    @stack('after-script')
 </body>
 
 </html>
