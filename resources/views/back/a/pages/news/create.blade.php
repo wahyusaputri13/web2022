457edc097530558618176886a33ef8742fde0b0d
isamaulanatantra <isamaulanatantra@gmail.com>
2023-08-22 09:35:22 +0700

    ganti versi ckeditor

--- resources/views/back/a/pages/news/create.blade.php
+++ resources/views/back/a/pages/news/create.blade.php
@@ -69,18 +69,18 @@
 </script>
 
 <!-- ck editor -->
-<!-- <script src="{{ asset('assets/back/assets/ckeditor/ckeditor.js') }}"></script> -->
-<!-- <script>
+<script src="{{asset('assets/back/assets/ckeditor/ckeditor.js')}}"></script>
+<script>
+    var konten = document.getElementById("my-editor");
     var options = {
         filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
         filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
         filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
         filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
     };
+    CKEDITOR.replace(konten, options);
+    CKEDITOR.config.allowedContent = true;
 </script>
-<script>
-    CKEDITOR.create('my-editor', options);
-</script> -->
 <!-- end ck editor -->
 
 <!-- tiny mce editor -->
