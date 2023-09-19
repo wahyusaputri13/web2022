8423ee1357ec4b1babbf9f09eecf78e27b1574cb
isamaulanatantra <isamaulanatantra@gmail.com>
2023-09-19 15:26:19 +0700

    c

--- resources/views/front/includes/footer.blade.php
+++ resources/views/front/includes/footer.blade.php
@@ -7,12 +7,12 @@
 
                 <div class="col-lg-4 col-md-6">
                     <div class="footer-info">
-                        <h3>UpConstruction</h3>
+                        <h3>{{ $data_website->web_name }}</h3>
                         <p>
-                            A108 Adam Street <br>
-                            NY 535022, USA<br><br>
-                            <strong>Phone:</strong> +1 5589 55488 55<br>
-                            <strong>Email:</strong> info@example.com<br>
+                            {{ $data_website->address }} <br>
+                            <br><br>
+                            <strong>Telepon:</strong> {{ $data_website->phone }}<br>
+                            <strong>Email:</strong> {{ $data_website->email }}<br>
                         </p>
                         <div class="social-links d-flex mt-3">
                             <a href="#" class="d-flex align-items-center justify-content-center"><i
@@ -22,54 +22,25 @@
                             <a href="#" class="d-flex align-items-center justify-content-center"><i
                                     class="bi bi-instagram"></i></a>
                             <a href="#" class="d-flex align-items-center justify-content-center"><i
-                                    class="bi bi-linkedin"></i></a>
+                                    class="bi bi-youtube"></i></a>
                         </div>
                     </div>
-                </div><!-- End footer info column-->
+                </div>
+                <!-- End footer info column-->
 
                 <div class="col-lg-2 col-md-3 footer-links">
-                    <h4>Useful Links</h4>
+                    <h4>Link Terkait</h4>
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About us</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Terms of service</a></li>
-                        <li><a href="#">Privacy policy</a></li>
+                        @foreach($related ?? [] as $rr)
+                        <li>
+                            <a href="{{ $rr->url }}" target="_blank">
+                                {{ $rr->name }}
+                            </a>
+                        </li>
+                        @endforeach
                     </ul>
-                </div><!-- End footer links column-->
-
-                <div class="col-lg-2 col-md-3 footer-links">
-                    <h4>Our Services</h4>
-                    <ul>
-                        <li><a href="#">Web Design</a></li>
-                        <li><a href="#">Web Development</a></li>
-                        <li><a href="#">Product Management</a></li>
-                        <li><a href="#">Marketing</a></li>
-                        <li><a href="#">Graphic Design</a></li>
-                    </ul>
-                </div><!-- End footer links column-->
-
-                <div class="col-lg-2 col-md-3 footer-links">
-                    <h4>Hic solutasetp</h4>
-                    <ul>
-                        <li><a href="#">Molestiae accusamus iure</a></li>
-                        <li><a href="#">Excepturi dignissimos</a></li>
-                        <li><a href="#">Suscipit distinctio</a></li>
-                        <li><a href="#">Dilecta</a></li>
-                        <li><a href="#">Sit quas consectetur</a></li>
-                    </ul>
-                </div><!-- End footer links column-->
-
-                <div class="col-lg-2 col-md-3 footer-links">
-                    <h4>Nobis illum</h4>
-                    <ul>
-                        <li><a href="#">Ipsam</a></li>
-                        <li><a href="#">Laudantium dolorum</a></li>
-                        <li><a href="#">Dinera</a></li>
-                        <li><a href="#">Trodelas</a></li>
-                        <li><a href="#">Flexo</a></li>
-                    </ul>
-                </div><!-- End footer links column-->
+                </div>
+                <!-- End footer links column-->
 
             </div>
         </div>
@@ -78,15 +49,14 @@
     <div class="footer-legal text-center position-relative">
         <div class="container">
             <div class="copyright">
-                &copy; Copyright <strong><span>UpConstruction</span></strong>. All Rights Reserved
+                &copy; <strong><span>2023 DISKOMINFO WONOSOBO</span></strong>.
             </div>
             <div class="credits">
                 <!-- All the links in the footer should remain intact. -->
                 <!-- You can delete the links only if you purchased the pro version. -->
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> Distributed by <a
-                    href="https://themewagon.com">ThemeWagon</a>
+                by <a href="https://soulofjava.github.io/myportofolio/">Isa Maulana Tantra</a>
             </div>
         </div>
     </div>
--- resources/views/front/includes/style.blade.php
+++ resources/views/front/includes/style.blade.php
@@ -1,6 +1,6 @@
 <!-- Favicons -->
-<link href="{{ asset('assets/front/assets/img/favicon.png') }}" rel="icon">
-<link href="{{ asset('assets/front/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+<link href="{{ asset('assets/pemda.ico') }}" rel="icon">
+<link href="{{ asset('assets/pemda.ico') }}" rel="apple-touch-icon">
 
 <!-- Google Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
