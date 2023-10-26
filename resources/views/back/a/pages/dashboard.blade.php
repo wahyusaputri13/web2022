b8c46e3fcc30fef8a3155a134c4ec277747ea524
isamaulanatantra <isamaulanatantra@gmail.com>
2023-10-26 11:07:39 +0700

    perbaiki data

--- resources/views/back/a/pages/dashboard.blade.php
+++ resources/views/back/a/pages/dashboard.blade.php
@@ -21,25 +21,6 @@
                     </div>
                 </div>
             </div>
-            @can('satpolpp')
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="card card-stats">
-                    <div class="card-header" data-background-color="blue">
-                        <i class="material-icons">assignment</i>
-                    </div>
-                    <div class="card-content">
-                        <p class="category">Public Complaints</p>
-                        <h3 class="card-title">{{ $public_complaints }}</h3>
-                    </div>
-                    <div class="card-footer">
-                        <!-- <div class="stats">
-                            <i class="material-icons text-danger">warning</i>
-                            <a href="#pablo">Get More Space...</a>
-                        </div> -->
-                    </div>
-                </div>
-            </div>
-            @endcan
             <div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="card card-stats">
                     <div class="card-header" data-background-color="purple">
