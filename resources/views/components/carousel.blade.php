2c60f3b307fb4517b89517847d8f50f2481f460b
isamaulanatantra <isamaulanatantra@gmail.com>
2023-11-07 14:19:43 +0700

    thumb

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,14 +1,29 @@
 <div id="carouselExample" class="carousel slide">
     <div class="carousel-inner">
         @forelse($jjj->gambar as $gambar)
+
         @if($loop->iteration == 1)
+
+        @if(Str::contains($gambar->path, 'https'))
+        <div class="carousel-item active">
+            <img src="{{ $gambar->path }}" class="d-block w-100" alt="{{ $gambar->file_name }}">
+        </div>
+        @else
         <div class="carousel-item active">
             <img src="{{  url('storage/')}}/{{ $gambar->path }}" class="d-block w-100" alt="{{ $gambar->file_name }}">
+        </div>
+        @endif
+        @else
+        @if(Str::contains($gambar->path, 'https'))
+        <div class="carousel-item">
+            <img src="{{ $gambar->path }}" class="d-block w-100" alt="{{ $gambar->file_name }}">
         </div>
         @else
         <div class="carousel-item">
             <img src="{{ url('storage/')}}/{{ $gambar->path }}" class="d-block w-100" alt="{{ $gambar->file_name }}">
         </div>
+        @endif
+
         @endif
         @empty
         <div class="carousel-item active">
